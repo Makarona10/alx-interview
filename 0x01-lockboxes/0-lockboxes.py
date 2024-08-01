d5ca0338d31a1075b0c4a993be2bb6b4c280cd88
Makarona10 <ahmedneyssi@gmail.com>
2024-08-02 02:51:48 +0300

    Problem solved

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,6 +7,7 @@ def canUnlockAll(boxes):
     '''Determines if all the boxes can be opened'''
     keys = [0]
     boxesNumber = len(boxes)
+    if boxesNumber == 0: return False
     for i in range(boxesNumber):
         for key in boxes[i]:
             if key != i and key not in keys:
