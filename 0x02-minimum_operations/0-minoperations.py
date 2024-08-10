b06f2d836460b9fed4a1ec5d926f1f6c747779af
Makarona10 <ahmedneyssi@gmail.com>
2024-08-10 06:29:43 +0300

    Task 0 done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,17 +9,25 @@ def isPrime(n: int) -> bool:
             return False
     return True
 
+
 def minOperations(n: int) -> int:
     if (type(n) is not int) or (n <= 1):
         return 0
     if isPrime(n):
         return n
-    if isPrime(n/2):
-        return int(n/2) + 2
     if n % 2 == 0:
-        return 1 + minOperations(n / 2)
-    # else:
-    #     steps = n
-    #     for i in range(1, )
-
-print(minOperations(28))
+        if isPrime(n // 2):
+            return int(n // 2) + 2
+        return 2 + minOperations(n // 2)
+    else:
+        steps = 2
+        step = 1
+        i = 2
+        while (i < n):
+            if n % i == 0:
+                step = i
+                steps += 2
+                i *= 2
+            i += step
+            steps += 1
+    return steps
