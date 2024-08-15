0c8277259907f69b79746bffb4691714b0d15282
Makarona10 <ahmedneyssi@gmail.com>
2024-08-15 23:26:41 +0300

    Task done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,15 +7,14 @@ from sys import stdin
 
 if __name__ == '__main__':
     stdin = stdin
-    lines = []
+    lines = 0
     fileSize = 0
     statusCodes = {'200': 0, '301': 0, '400': 0,
                    '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
     try:
         for line in stdin:
-            lines.append(line)
-            fileSize += int(((line.split())[-1]))
+            lines += 1
             try:
                 fileSize += int(((line.split())[-1]))
                 if (line.split())[-2] in list(statusCodes.keys()):
@@ -23,7 +22,7 @@ if __name__ == '__main__':
             except (IndexError, ValueError):
                 pass
 
-            if len(lines) % 10 == 0:
+            if lines % 10 == 0:
                 print(f'File size: {fileSize}')
                 for k, val in statusCodes.items():
                     if val > 0:
