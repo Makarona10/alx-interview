43a0c43d9e3c5f911c6161be6729948b114f9ee9
Makarona10 <ahmedneyssi@gmail.com>
2024-08-15 18:48:34 +0300

    Task done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,27 +2,35 @@
 
 '''0x03. Log Parsing Problem'''
 
-import sys
+from sys import stdin
 
 
+stdin = stdin
 lines = []
 fileSize = 0
-statusCodes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-while True:
-    try:
-        line = sys.stdin.readline()
+statusCodes = {'200': 0, '301': 0, '400': 0,
+               '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+print(list(statusCodes.keys()))
+try:
+    for line in stdin:
         lines.append(line)
-        fileSize += int(((line.split(' '))[-1]).strip())
-        status = int((line.split(' '))[-2])
-        if isinstance(status, int) or status in list(statusCodes.keys()):
-            statusCodes[status] += 1
-            if len(lines) % 10 == 0:
-                print(f'File size: {fileSize}')
-                for k, val in statusCodes.items():
-                    if val > 0:
-                        print(f'{k}: {val}')
-    except KeyboardInterrupt:
-        print(f'File size: {fileSize}')
-        for k, val in statusCodes.items():
-            if val > 0:
-                print(f'{k}: {val}')
+        status = int((line.split())[-2])
+        fileSize += int(((line.split())[-1]))
+        try:
+                fileSize += int(((line.split())[-1]))
+                if (line.split())[-2] in list(statusCodes.keys()):
+                    statusCodes[(line.split())[-2]] += 1
+        except (IndexError, ValueError):
+            pass
+       
+        if len(lines) % 10 == 0:
+            print(f'File size: {fileSize}')
+            for k, val in statusCodes.items():
+                if val > 0:
+                    print(f'{k}: {val}')
+except KeyboardInterrupt:
+    print(f'File size: {fileSize}')
+    for k, val in statusCodes.items():
+        if val > 0:
+            print(f'{k}: {val}')
+    raise
