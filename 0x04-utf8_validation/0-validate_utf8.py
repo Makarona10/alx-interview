926262b1f409a9f4a45c0b731b7246cd458a795a
Makarona10 <ahmedneyssi@gmail.com>
2024-08-22 19:07:03 +0300

    Task done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,17 +9,17 @@ def validUTF8(data):
         if data[idx] & 192 <= 128:
             idx += 1
             continue
-        elif (data[idx] & 224 == 192 and idx+1 < len(data)
-              and data[idx+1] & 192 == 128):
+        elif (data[idx] & 224 == 192 and idx+1 < len(data) and
+              data[idx+1] & 192 == 128):
             idx += 2
             continue
-        elif (data[idx] & 240 == 224 and idx+2 < len(data)
-              and data[idx+1] & 192 == 128
-              and data[idx+2] & 192 == 128):
+        elif (data[idx] & 240 == 224 and idx+2 < len(data) and
+              data[idx+1] & 192 == 128 and
+              data[idx+2] & 192 == 128):
             idx += 3
             continue
-        elif (data[idx] & 248 == 240 and idx+3 < len(data)
-              and data[idx+1] & 192 == 128 and
+        elif (data[idx] & 248 == 240 and idx+3 < len(data) and
+              data[idx+1] & 192 == 128 and
               data[idx+2] & 192 == 128 and data[idx+3] & 192 == 128):
             idx += 4
             continue
