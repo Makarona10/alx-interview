dc808a0153b8a8cfad09a027623f035d42696aba
Makarona10 <ahmedneyssi@gmail.com>
2024-08-22 18:34:22 +0300

    Task done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" 0x04. UTF-8 Validation """
-
+'''0x04. UTF-8 Validation Project'''
 
 from typing import List
 
@@ -10,7 +9,7 @@ def validUTF8(data: List[int]) -> bool:
 
     idx = 0
     while idx < len(data):
-        if data[idx] & 192 <= 128 and data[idx] > 0:
+        if data[idx] & 192 <= 128:
             idx += 1
             continue
         elif (data[idx] & 224 == 192 and idx+1 < len(data)
@@ -18,13 +17,13 @@ def validUTF8(data: List[int]) -> bool:
             idx += 2
             continue
         elif (data[idx] & 240 == 224 and idx+2 < len(data)
-              and data[idx+1] & 224 == 192
+              and data[idx+1] & 192 == 128
               and data[idx+2] & 192 == 128):
             idx += 3
             continue
         elif (data[idx] & 248 == 240 and idx+3 < len(data)
-              and data[idx+1] & 240 == 224 and
-              data[idx+2] & 224 == 192 and data[idx+3] & 192 == 128):
+              and data[idx+1] & 192 == 128 and
+              data[idx+2] & 192 == 128 and data[idx+3] & 192 == 128):
             idx += 4
             continue
         else:
