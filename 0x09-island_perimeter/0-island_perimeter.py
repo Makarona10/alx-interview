10d0d0d7ba06284565c8b7acad85066f03e986c1
Makarona10 <ahmedneyssi@gmail.com>
2024-09-25 02:25:45 +0300

    Problem solved

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,15 +5,30 @@
 def island_perimeter(grid) -> int:
     '''returns the perimeter of the island described in grid'''
     ribs = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
+    rows = len(grid)
+    columns = len(grid[0])
+
+    for row in range(rows):
+        for col in range(columns):
             if grid[row][col] == 1:
-                if grid[row-1][col] == 0:
+                if row > 0:
+                    if grid[row-1][col] == 0:
+                        ribs += 1
+                else:
                     ribs += 1
-                if grid[row+1][col] == 0:
+                if row < rows - 1:
+                    if grid[row+1][col] == 0:
+                        ribs += 1
+                else:
                     ribs += 1
-                if grid[row][col+1] == 0:
+                if col > 0:
+                    if grid[row][col-1] == 0:
+                        ribs += 1
+                else:
                     ribs += 1
-                if grid[row][col-1] == 0:
+                if col < columns - 1:
+                    if grid[row][col+1] == 0:
+                        ribs += 1
+                else:
                     ribs += 1
     return ribs
