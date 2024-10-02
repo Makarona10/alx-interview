4f7371b201c63bdd09dc698b9779b42936734599
Makarona10 <ahmedneyssi@gmail.com>
2024-10-03 00:54:32 +0300

    problem solved

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+def square_root(val):
+    ans = 1
+    sqr = 1
+    i = 1
+    while (sqr <= val):
+        i += i
+        sqr = i * i
+    ans = i - 1
+    return ans
+
+
+def get_primes(n):
+    if n <= 2:
+        return 0
+
+    is_prime = [True] * n
+    is_prime[0] = False
+    is_prime[1] = False
+
+    for i in range(2, square_root(n)):
+        if is_prime[i]:
+            for x in range(i * i, n, i):
+                is_prime[x] = False
+
+    return len([i for i in range(n) if is_prime[i]])
+
+
+def isWinner(x, nums):
+    maria = 0
+    ben = 0
+    for i in range(x):
+        primes = get_primes(nums[i])
+        if primes % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    return 'Maria' if maria > ben else 'Ben'
