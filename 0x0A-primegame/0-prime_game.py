b02d863850cc1bf30634d1fc80af191102279b52
Makarona10 <ahmedneyssi@gmail.com>
2024-10-03 01:37:34 +0300

    Problem solved

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,33 +3,20 @@
 """0x0A. Prime Game problem"""
 
 
-def square_root(val):
-    """Gets the square root of a number (similar to math.isqrt)"""
-    ans = 1
-    sqr = 1
-    i = 1
-    while (sqr <= val):
-        i += i
-        sqr = i * i
-    ans = i - 1
-    return ans
-
-
 def get_primes(n):
     """Get a list of prime numbers less than or equal to a specific number n"""
     if n < 2:
         return []
 
-    is_prime = [True] * (n + 1)  # Include n
+    is_prime = [True] * (n + 1)
     is_prime[0] = False
     is_prime[1] = False
 
     for i in range(2, int(n ** 0.5) + 1):
         if is_prime[i]:
-            for x in range(i * i, n + 1, i):  # Ensure multiples of i are marked
+            for x in range(i * i, n + 1, i):
                 is_prime[x] = False
 
-    # return [i for i in range(n + 1) if is_prime[i]]
     return len([i for i in range(n) if is_prime[i]])
 
 
